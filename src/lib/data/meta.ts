ac7600e4e8708b7236678623917a71b53d3e482c
JunhoHwang1 <junho.h@dvmon.com>
2023-11-02 22:49:16 +0900

    fix(seo): 메타 데이터 변경

--- src/lib/data/meta.ts
+++ src/lib/data/meta.ts
@@ -7,15 +7,16 @@ export const siteBaseUrl = 'https://hjh3311504.github.io';
 export const keywords = [
 	'Svelte',
 	'SvelteKit',
-	'Template',
+	'스벨트',
+	'스벨트킷',
 	'Blog',
-	'Starter',
-	'Static Site'
+	'블로그',
+	'developer',
+	'개발자',
 ];
 
-export const description =
-	"A light, neat, and easy-to-use SvelteKit template for your next website.";
+export const description = "황준호의 개발 블로그입니다. 잘 부탁드립니다!!";
 
-export const title = 'SvelteKit Static Blog Template';
+export const title = 'Juno\'s develog';
 
 export const image = `${siteBaseUrl}/images/site-preview.png`;
